61f3a3a5613ad4277d0cbb2813b8dd27ad402230
a2623212 <a2623212@gmail.com>
2024-01-29 22:50:22 +0900

    feat: rerun

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import *
 import os
-# from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,23 +11,18 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.getenv('YOUR_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('YOUR_CHANNEL_SECRET'))
 
-# client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
 def generate_response(prompt, role="user"):
     pass
 
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
-
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
@@ -38,5 +32,7 @@ def handle_message(event):
         TextSendMessage(text=event.message.text)
     )
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
