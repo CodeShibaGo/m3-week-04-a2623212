c9ff907c4910eeb8562a1330776d79c236fc7e1b
a2623212 <a2623212@gmail.com>
2024-01-30 11:50:42 +0900

    feat: add openAI

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
+from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -10,9 +11,12 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv('YOUR_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('YOUR_CHANNEL_SECRET'))
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 def generate_response(prompt, role="user"):
     pass
+
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -27,10 +31,24 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # 加上openAI的回覆
+    if event.message.text == '你是AI嗎？':
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Hello!"}
+        ]
+        )
+        print(completion.choices[0].message)
+        text= response['choices'][0]['message']['content']
+    else:
+        # 原本的回覆
+        text=event.message.text
     line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+                event.reply_token,
+                TextSendMessage(text)
+            )
 
 import os
 if __name__ == "__main__":
