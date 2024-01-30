6ee06c3ce379f19467d85faa1f2c3d68d756a280
a2623212 <a2623212@gmail.com>
2024-01-30 12:09:02 +0900

    test logging

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def handle_message(event):
         ]
         )
         print(completion.choices[0].message)
-        text= ['choices'][0]['message']['content']
+        text= completion['choices'][0]['message']['content']
     else:
         # 原本的回覆
         text=event.message.text
